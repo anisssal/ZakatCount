903d9df5fc0cbbb9fe806229ed05a4ea50a600a6
anisssal <anis.alathiif@gmail.com>
2018-01-21 09:53:31 +0700

    Initial commit

---
+++ app/src/main/java/com/iman/inc/zakatcount/Count_ZF.java
@@ -0,0 +1,13 @@
+package com.iman.inc.zakatcount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Count_ZF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count__zf);
+    }
+}
